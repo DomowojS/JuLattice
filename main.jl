cc09c6b8191fd057121a1f09670047f7e6b50257
Daniel Sukhman <daniel.sukhman@hvl.no>
2025-05-02 15:13:40 +0200

    Add cylinder object as bounce back BC

--- main.jl
+++ main.jl
@@ -1,17 +1,20 @@
 ############################
 ## Main file for JuLattice #
 ############################
-using Revise
+using Revise, MeshGrid
 
 # User Settings
 gridlengthX  = 150;
 gridlengthY  = 50;
 
-fluiddensity      = 100;
+fluiddensity   = 100;
 
 simulationTime = 1000;
 
-# Simulation Settings
+cylinder_radius  = 10;
+cylinder_position = [gridlengthX/3, gridlengthY/2]
+
+## Simulation Settings
 Q   = 9;
 τ   = 1/√3 
 
@@ -24,18 +27,25 @@ lattice_vely = reshape(lattice_velocity_unit[2,],1,1,Q)
 weights =   [4/9, 1/9, 1/9, 1/9, 1/9, 1/36, 1/36, 1/36, 1/36];
 weights =   reshape(weights,1,1,Q);
 
+# create grid
+gridX, gridY = meshgrid(1:gridlengthX, 1:gridlengthY);
+gridX, gridY = gridX', gridY';
+
+# create object indetifier
+cylinder = (gridX.-cylinder_position[1]).^2 + (gridY.-cylinder_position[2]).^2 .< cylinder_radius.^2;
+
 # Initialize distributions arrays
 distributions = ones(gridlengthX, gridlengthY, Q) .+ 0.01*rand(gridlengthX, gridlengthY, Q);
+distributions[:,:,4] .+= 2 .* (1 .+ 0.2 .* cos.(2 .* π .*gridX ./ gridlengthX .*4));
 distributions_equilibrium = ones(gridlengthX, gridlengthY, Q);
-distributions_streamed = ones(gridlengthX, gridlengthY, Q);
 
 # Initialize macroscopic density and scale distribution
 densityGrid = sum(distributions, dims=3);
 distributions .*= fluiddensity ./ densityGrid;
 
 # Initialize macroscopic velocity arrays
-velocityX   = zeros(gridlengthX, gridlengthY, Q);
-velocityY   = zeros(gridlengthX, gridlengthY, Q);
+velocityX   = zeros(gridlengthX, gridlengthY);
+velocityY   = zeros(gridlengthX, gridlengthY);
 
 # Initialise dotproduct array 
 dotprod_velocities = zeros(gridlengthX, gridlengthY, Q);
@@ -62,8 +72,8 @@ for i in 1:simulationTime
 
     # Apply Boundary conditions
 
-    # Apply object
-
+    # Apply object boundary condition
+    distributions[cylinder, 1:Q] .= distributions[cylinder, [1,4,5,2,3,8,9,6,7]];
 
 end
 
