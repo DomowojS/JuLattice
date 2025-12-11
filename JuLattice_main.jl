701ffde30746dab60f43d1927b134934a007f45d
Sören Blumberg <soeren.blumberg@tu-braunschweig.de>
2025-12-11 12:51:08 +0100

    change plotting to slice plotting for v_x and |vorticity|; added first try of dynamic window positioning

--- JuLattice_main.jl
+++ JuLattice_main.jl
@@ -10,12 +10,12 @@ using .Plotter, .Logger
 ##-------- User Settings --------##
 # Domain Settings
 length_X = 2              # m
-length_Y = 1              # m 
+length_Y = 0.5            # m 
 length_Z = 1              # m
 
 # sphere Definition
 Radius   = 0.1    # m
-Position = [1, 0.5, 0.5] # m [x,y,z]
+Position = [0.5, 0.25, 0.5] # m [x,y,z]
 
 # Fluid Settings 
 Fluid_Density = 1000.0;       # kg/m^3
@@ -24,16 +24,17 @@ Kinematic_Viscosity = 0.001; # m^2/s
 
 # Simulation Settings
 # Simulation_Time = 8000;     # s
-Simulation_Time = 10;     # s
+Simulation_Time = 5;     # s
 delta_x = 0.01;             # discretisation in time and space
-τ = 0.65;
-Re = (Inflow_Velocity .* Radius)/Kinematic_Viscosity;
+τ = 0.55 #0.65;
+#Re = (Inflow_Velocity .* Radius)/Kinematic_Viscosity;
+Re = (Inflow_Velocity .* 2 .* Radius)/Kinematic_Viscosity;
 Re_Log=floor(Int,Re)
 
 # Plot Requests (Flags)
-Plotvx = true;
-Plotvy = true;
-Plotvz = true;
+Plotvx = false;
+Plotvy = false;
+Plotvz = false;
 Plotvorticity = true;
 
 
@@ -70,28 +71,33 @@ Log_Discretization_Settings(delta_x, delta_t, lattice_Re_Log)
 
 # Simulation Settings
 simulationTime = ceil(Int, Simulation_Time / delta_t);
-#Q   = 9;
-Q = 19; #D3Q19
 
-# velocity_vector = [     [0, -1, 0, 1, 0, -1, -1, 1, 1],
-#                         [0, 0, 1, 0, -1, -1, 1, 1, -1]];
+Q = 19; #D3Q19
+#link Hand Koordinatensystem
+# velocity_vector = [
+#     [0, 1, -1, 0,  0, 0,  0, 1, -1,  1, -1, 0,  0,  0,  0, 1, -1, -1,  1],        #x
+#     [0, 0,  0, 0,  0, 1, -1, 0,  0,  0,  0, 1, -1, -1,  1, 1, -1,  1, -1],        #y
+#     [0, 0,  0, 1, -1, 0,  0, 1, -1, -1,  1, 1, -1,  1, -1, 0,  0,  0,  0]         #z
+# ];
+
+#rechte Hand Koordinatensystem
 velocity_vector = [
-    [0, 1, -1, 0,  0, 0,  0, 1, -1,  1, -1, 0,  0,  0,  0, 1, -1, -1,  1],        #x
-    [0, 0,  0, 0,  0, 1, -1, 0,  0,  0,  0, 1, -1, -1,  1, 1, -1,  1, -1],        #y
-    [0, 0,  0, 1, -1, 0,  0, 1, -1, -1,  1, 1, -1,  1, -1, 0,  0,  0,  0]         #z
+    [0, 1, -1, 0,  0,  0, 0, 1, -1,  1, -1,  0, 0, 0,  0,  1, -1, -1, 1],        #x
+    [0, 0,  0, 0,  0, -1, 1, 0,  0,  0,  0, -1, 1, 1, -1, -1,  1, -1, 1],        #y
+    [0, 0,  0, 1, -1,  0, 0, 1, -1, -1,  1,  1,-1, 1, -1,  0,  0,  0, 0]         #z
 ];
+
 
 #define velocity vectors for all directions
 velocity_vector_x = reshape(velocity_vector[1,],1,1,1,Q)
 velocity_vector_y = reshape(velocity_vector[2,],1,1,1,Q)
 velocity_vector_z = reshape(velocity_vector[3,],1,1,1,Q)
 
-# weights =   [4/9, 1/9, 1/9, 1/9, 1/9, 1/36, 1/36, 1/36, 1/36];
-# weights =   reshape(weights,1,1,Q);
-weights = [2/9,                                      #ruhe (1)
+
+weights = [3/9,                                      #ruhe (1)
             1/18, 1/18, 1/18, 1/18, 1/18, 1/18,      #axial (6)
             1/36, 1/36, 1/36, 1/36, 1/36, 1/36,      #diagonal (12)
-            1/36, 1/36, 1/36, 1/36, 1/36, 1/36, ];   #-------------19
+            1/36, 1/36, 1/36, 1/36, 1/36, 1/36];     #-------------19
 
 weights = reshape(weights, 1, 1, 1, Q);
 
@@ -104,6 +110,10 @@ if size(gridX) == (gridlengthY, gridlengthX, gridlengthZ) #check for format of g
     gridY = permutedims(gridY, (2,1,3))
     gridZ = permutedims(gridZ, (2,1,3))
 end    
+
+midY = ceil(Int, gridlengthY/2)
+midZ = ceil(Int, gridlengthZ/2)
+
 # #---- Debug
 # @show gridlengthX, gridlengthY, gridlengthZ
 # @show size(gridX), size(gridY), size(gridZ)
@@ -113,13 +123,8 @@ end
 # flush(stdout)
 # error("DEBUG STOP")
 # #---- Debug
-#Umformen damit für spätere Berechnungen im richtigen Format
-#gridX = gridX'
-#gridY = gridY'
-
 
 # create object indetifier
-# cylinder = (gridX.-cylinder_position[1]).^2 + (gridY.-cylinder_position[2]).^2 .< cylinder_radius.^2;
 sphere = (gridX .- sphere_position[1]).^2 + (gridY .- sphere_position[2]).^2 + (gridZ .- sphere_position[3]).^2 .< sphere_radius.^2
 
 # create boundary indetifiers
@@ -127,25 +132,14 @@ walls = gridY .== 1 .| gridY .== gridlengthY .| gridZ .==1 .| gridZ .== gridleng
 inlet = gridX .== 1;
 outlet = gridX .== gridlengthX;
 
-# # Initialize distributions arrays 2D
-# distributions = ones(gridlengthX, gridlengthY, Q) .+ 0.01*rand(gridlengthX, gridlengthY, Q);
-# distributions[:,:,4] .+= 2 .* (1 .+ 0.2 .* cos.(2 .* π .*gridX ./ gridlengthX .*4));
-# distributions_equilibrium = ones(gridlengthX, gridlengthY, Q);
-
 # Initialize distributions arrays 3D
 distributions = ones(gridlengthX, gridlengthY, gridlengthZ, Q) .+ 0.01*rand(gridlengthX, gridlengthY, gridlengthZ, Q);
-distributions[:,:,:,4] .+= 2 .* (1 .+ 0.2 .* cos.(2 .* π .* gridX ./ gridlengthX .*4));
+distributions[:,:,:,2 ] .+= 2 .* (1 .+ 0.2 .* cos.(2 .* π .* gridX ./ gridlengthX .*4));
 distributions_equilibrium = ones(gridlengthX, gridlengthY, gridlengthZ, Q);
-
-
-# # Initialize macroscopic density and scale distribution 2D
-# densityGrid = sum(distributions, dims=3);
-# distributions .*= fluiddensity ./ densityGrid;
 
 # Initialize macroscopic density and scale distribution 3D
 densityGrid = sum(distributions, dims=4);
 distributions .*= fluiddensity ./ densityGrid;
-
 
 # Initialize macroscopic velocity arrays
 velocityX   = zeros(gridlengthX, gridlengthY, gridlengthZ);
@@ -160,40 +154,71 @@ omegaMag = zeros(gridlengthX, gridlengthY, gridlengthZ)
 # Initialise dotproduct array 
 dotprod_velocities = zeros(gridlengthX, gridlengthY, gridlengthZ, Q);
 
-#Initialise omega
-omega = zeros(gridlengthX, gridlengthY , gridlengthZ)
-
 #Plot calls
 if any((Plotvorticity, Plotvx, Plotvy, Plotvz))
-    if Plotvorticity == true
-        omegaMag_obs, step_text_omega, fig_omega = Create_Plot3D(gridlengthX, gridlengthY, gridlengthZ, omegaMag; title="|ω|")
-        screenOmega = GLMakie.Screen()
-        display(screenOmega, fig_omega)
+    # #3D Plot
+    # if Plotvorticity == true
+    #     omegaMag_obs, step_text_omega, fig_omega = Create_Plot3D(gridlengthX, gridlengthY, gridlengthZ, omegaMag; title="|ω|")
+    #     screenOmega = GLMakie.Screen()
+    #     display(screenOmega, fig_omega)
+    # end
+    
+    # Bildschirmgröße ermitteln
+    #Default
+    screen_width = 1920
+    screen_height = 1080
+
+    try
+        monitor = GLMakie.GLFW.GetPrimaryMonitor()
+        mode = GLMakie.GLFW.GetVideoMode(monitor)
+        global screen_width = mode.width
+        global screen_height = mode.height
+    catch
     end
 
-    # if Plotvorticity==true 
-    #     vorticity, vorticity_obs, text_obj, step_text, fig_vorticity = Create_Plot(gridlengthX, gridlengthY)
-    #     screen1 = GLMakie.Screen()
-    #     GLMakie.display(screen1, fig_vorticity)
-    # end
+    gap = 20
+    window_width = Int(floor((screen_width - 3*gap) / 2))
+    window_height = Int(floor((screen_height - 3*gap) / 2))
 
-    # if Plotvx==true 
-    #     velocityX_obs, text_obj_vx, step_text_vx, fig_vx = Create_Plot(gridlengthX, gridlengthY, velocityX, "X")
-    #     screen2 = GLMakie.Screen(; position = (600, 0))
-    #     GLMakie.display(screen2, fig_vx)
-    # end
+    ###DEBUG###
+    println("Screen Resolution: $(screen_width) x $(screen_height)")
 
-    # if Plotvy==true 
-    #     velocityY_obs, text_obj_vy, step_text_vy, fig_vy = Create_Plot(gridlengthX, gridlengthY, velocityY, "Y")
-    #     screen3 = GLMakie.Screen()
-    #     display(screen3, fig_vy)
-    # end
+    if Plotvx==true
+        #xy slice at z=midZ
+        vx_xy_obs, step_text_vx_xy, fig_vx_xy = Create_Plot_XY(gridlengthX, gridlengthY, velocityX[:,:,midZ]; width=window_width, height=window_height, title="v_x at z=$(midZ)")
+        screen_vx_xy = GLMakie.Screen(position=(gap, gap))
+        display(screen_vx_xy, fig_vx_xy)
 
-    # if Plotvz==true
-    #     velocityZ_obs, text_obj_vz, step_text_vz, fig_vz = Create_Plot(gridlengthX, gridlengthZ, velocityZ, "Z")
-    #     screen4 = GLMakie.Screen()
-    #     display(screen4, fig_vz)
-    # end
+        #xz slice at y=midY
+        vx_xz_obs, step_text_vx_xz, fig_vx_xz = Create_Plot_XZ(gridlengthX, gridlengthZ, velocityX[:,midY,:]; width=window_width, height=window_height, title="v_x at y=$(midY)")
+        screen_vx_xz = GLMakie.Screen(position=(window_width + 2*gap, gap))
+        display(screen_vx_xz, fig_vx_xz)
+    end
+
+    if Plotvorticity == true
+        #xy slice at z=midZ
+        omega_xy_obs, step_text_omega_xy, fig_omega_xy = Create_Vorticity_XY(gridlengthX, gridlengthY, omegaMag[:,:,midZ]; width=window_width, height=window_height, title = "|ω| at z=$(midZ)")
+        screen_omega_xy = GLMakie.Screen(position=(gap, window_height + 2*gap))
+        display(screen_omega_xy, fig_omega_xy)
+
+        #xz slice at y=midY
+        omega_xz_obs, step_text_omega_xz, fig_omega_xz = Create_Vorticity_XZ(gridlengthX, gridlengthZ, omegaMag[:,midY,:]; width=window_width, height=window_height, title = "|ω| at y=$(midY)")
+        screen_omega_xz = GLMakie.Screen(position=(window_width + 2*gap, window_height + 2*gap))
+        display(screen_omega_xz, fig_omega_xz)    
+    end
+
+
+    if Plotvy==true 
+        velocityY_obs, text_obj_vy, step_text_vy, fig_vy = Create_Plot(gridlengthX, gridlengthY, velocityY, "Y")
+        screen3 = GLMakie.Screen()
+        display(screen3, fig_vy)
+    end
+
+    if Plotvz==true
+        velocityZ_obs, text_obj_vz, step_text_vz, fig_vz = Create_Plot(gridlengthX, gridlengthZ, velocityZ, "Z")
+        screen4 = GLMakie.Screen()
+        display(screen4, fig_vz)
+    end
 
 end
 
@@ -202,39 +227,23 @@ println("Starting Simulation:")
 # Run Simulation Loop
 for i in 1:simulationTime
 
-    # # Get Macroscopic values 2D
-    # global densityGrid = sum(distributions, dims=3);
-    # velocityX .= (1 ./ densityGrid) .* sum(distributions.*velocity_vector_x, dims=3); 
-    # velocityY .= (1 ./ densityGrid) .* sum(distributions.*velocity_vector_y, dims=3); 
-
     # Get Macroscopic values 3D
     global densityGrid = sum(distributions, dims=4);
     velocityX .= (1 ./ densityGrid) .* sum(distributions.*velocity_vector_x, dims=4); 
     velocityY .= (1 ./ densityGrid) .* sum(distributions.*velocity_vector_y, dims=4); 
     velocityZ .= (1 ./ densityGrid) .* sum(distributions.*velocity_vector_z, dims=4); 
 
-    # #vorticity
-    # dv_dx = (circshift(velocityY, (-1,0,0)) .- circshift(velocityY, (1,0,0))) ./ 2
-    # dw_dx = (circshift(velocityZ, (-1,0,0)) .- circshift(velocityZ, (1,0,0))) ./ 2
+    #DEBUGGING############
+    if i % 20 == 0
+        @views begin
+            xy_min, xy_max = extrema(velocityX[:,:,midZ])
+            xz_min, xz_max = extrema(velocityX[:,midY,:])
+        end
 
-    # du_dy = (circshift(velocityX, (0,-1,0)) .- circshift(velocityX, (0,1,0))) ./ 2
-    # dw_dy = (circshift(velocityZ, (0,-1,0)) .- circshift(velocityZ, (0,1,0))) ./ 2
-    
-    # du_dz = (circshift(velocityX, (0,0,-1)) .- circshift(velocityX, (0,0,1))) ./ 2
-    # dv_dz = (circshift(velocityY, (0,0,-1)) .- circshift(velocityY, (0,0,1))) ./ 2
+    println("vx XY min/max : ($(xy_min), $(xy_max)) | XZ min/max: ($(xz_min), $(xz_max)) at step $i")
+    end
+    #DEBUGGING############
 
-    # omegaX .= dw_dy .- dv_dz
-    # omegaY .= du_dz .- dw_dx
-    # omegaZ .= dv_dx .-du_dy
-    # omegaMag .= sqrt.(omegaX.^2 .+ omegaY.^2 .+ omegaZ.^2)
-
-
-    # ## Apply Collision 2D
-    # # Compute equilibrium state
-    # dotprod_velocities .= (velocity_vector_x .* velocityX) .+ (velocity_vector_y .* velocityY);
-    # distributions_equilibrium .= weights .* densityGrid .*(1 .+ 3 .*dotprod_velocities .+ 4.5 .*dotprod_velocities.^2 .- 1.5 .*(velocityX.^2 .+ velocityY.^2));
-    # # Relax towards equilibrium
-    # distributions .+= -(1/τ) .* (distributions .- distributions_equilibrium);
 
     ## Apply Collision 3D
     # Compute equilibrium state
@@ -242,7 +251,6 @@ for i in 1:simulationTime
     distributions_equilibrium .= weights .* densityGrid .*(1 .+ 3 .*dotprod_velocities .+ 4.5 .*dotprod_velocities.^2 .- 1.5 .*(velocityX.^2 .+ velocityY.^2 + velocityZ.^2));
     # Relax towards equilibrium
     distributions .+= -(1/τ) .* (distributions .- distributions_equilibrium);
-
 
     # Stream 
     for j in 1:Q
@@ -262,15 +270,9 @@ for i in 1:simulationTime
     # distributions[outlet, [4, 8, 9]] .= distributions[gridlengthX-1, :, [4, 8, 9]]
     # ------------------------ vorerst ohne ------------------------ 
 
-    # #No Slip Walls 2D
-    # distributions[walls, 1:Q] .= distributions[walls, [1,4,5,2,3,8,9,6,7]];
-
     #No Slip Walls 3D
     distributions[walls, 1:Q] .= distributions[walls, [1,3,2,5,4,7,6,9,8,11,10,13,12,15,14,17,16,19,18]];
     
-    # # Apply object boundary condition 2D
-    # distributions[cylinder, 1:Q] .= distributions[cylinder, [1,4,5,2,3,8,9,6,7]];
-
     # Apply object boundary condition 3D
     distributions[sphere, 1:Q] .= distributions[sphere, [1,3,2,5,4,7,6,9,8,11,10,13,12,15,14,17,16,19,18]];
 
@@ -280,12 +282,6 @@ for i in 1:simulationTime
             velocityX[sphere] .= NaN
             velocityY[sphere] .= NaN
             velocityZ[sphere] .= NaN
-
-            # # Compute vorticity 2D
-            # fill!(vorticity, 0.0)
-            # dv_dx = circshift(velocityY, (-1, 0)) .- circshift(velocityY, (1, 0))
-            # du_dy = circshift(velocityX, (0, -1)) .- circshift(velocityX, (0, 1))
-            # vorticity .= dv_dx .- du_dy
 
             # # Compute vorticity 3D
             #vorticity
@@ -303,36 +299,46 @@ for i in 1:simulationTime
             omegaZ .= dv_dx .-du_dy
             omegaMag .= sqrt.(omegaX.^2 .+ omegaY.^2 .+ omegaZ.^2)
             
-
-
             if ((i % 100 == 0)) || (i == simulationTime)
                 Log_Simulation_Runtime(i, simulationTime)
             end
             # Update the observables
-            if Plotvorticity == true                       
+            if Plotvorticity == true   
+                                   
                 #Mask inlet outlet
                 omegaMag[inlet] .= 0.0
                 omegaMag[outlet] .= 0.0
                 # Mask the cylinder region
                 omegaMag[sphere] .= NaN
-
-                omegaMag_obs[] = Float32.(omegaMag)
-                step_text_omega[] = "Time step: $i, $(floor(Int, i*delta_t))s"
+                
+                # #3D update: 
+                # omegaMag_obs[] = Float32.(omegaMag)
+                # step_text_omega[] = "Time step: $i, $(floor(Int, i*delta_t))s"
+                
+                #2D update:
+                #xy at midZ
+                omega_xy_obs[] = copy(omegaMag[:,:,midZ])
+                step_text_omega_xy[] = "Time step: $i, $(floor(Int, i*delta_t))s" 
+                #xz at midY
+                omega_xz_obs[] = copy(omegaMag[:,midY,:])
+                step_text_omega_xz[] = "Time step: $i, $(floor(Int, i*delta_t))s"
 
             end
 
-            # if Plotvorticity==true 
-            #     vorticity_obs[] = copy(vorticity) 
-            #     step_text[] = "Time step: $i, $(floor(Int, i*delta_t))s"
-            # end
-            # if Plotvx==true 
-            #     velocityX_obs[] = copy(velocityX) 
-            #     step_text_vx[] = "Time step: $i, $(floor(Int, i*delta_t))s"
-            # end
-            # if Plotvy==true 
-            #     velocityY_obs[] = copy(velocityY) 
-            #     step_text_vy[] = "Time step: $i, $(floor(Int, i*delta_t))s"
-            # end
+            if Plotvx==true
+                #refresh xy slice at z=midZ
+                vx_xy_obs[] = copy(velocityX[:,:,midZ])
+                step_text_vx_xy[] = "Time step:$i, $(floor(Int, i*delta_t))s"
+
+                #refresh xz slice at y=midY
+                vx_xz_obs[] = copy(velocityX[:,midY,:])
+                step_text_vx_xz[] = "Time step:$i, $(floor(Int, i*delta_t))s"
+            end
+
+            if Plotvy==true 
+                velocityY_obs[] = copy(velocityY) 
+                step_text_vy[] = "Time step: $i, $(floor(Int, i*delta_t))s"
+            end
 
             yield()
             sleep(0.05)
--- src/Plotter.jl
+++ src/Plotter.jl
@@ -1,78 +1,167 @@
 module Plotter
 using GLMakie
-export Create_Plot, Create_Plot3D
+using Colors
+export Create_Plot_XY, Create_Plot_XZ, Create_Vorticity_XY, Create_Vorticity_XZ
 
 ## Set up Plot 
-    function Create_Plot(gridlengthX::Int64, gridlengthY::Int64)
-        ## Set up the figure and axis
-        # Initialize Plot arrays
-        vorticity = zeros(gridlengthX, gridlengthY);
-        vorticity_obs = Observable(vorticity);       
-        # Set up the figure and axis with explicit sizing
-        fig = Figure(size = (1000, 400))
-        ax = Axis(fig[1, 1], aspect = DataAspect(), title = "Vorticity")
+######################################################
 
-        # Display the vorticity field using a heatmap with dynamic color range
-        hm = heatmap!(ax, 1:gridlengthX, 1:gridlengthY, vorticity_obs, 
-                    colormap = :curl, 
-                    nan_color = :black,
-                    colorrange = (-0.2, 0.2))
-        Colorbar(fig[1, 2], hm, label = "Lattice_Vorticity")
-        rowsize!(fig.layout, 1, ax.scene.viewport[].widths[2])
-        # Set axis limits explicitly
-        xlims!(ax, 1, gridlengthX)
-        ylims!(ax, 1, gridlengthY)
-
-        # Create a text element for time step display
-        step_text = Observable("Time step: 0, 0s")
-        text_obj = text!(ax, ceil(Int, (gridlengthX*2/100)), ceil(Int, (gridlengthY*2/100)), text = step_text, 
-                color = :black, fontsize = 14)
-                return vorticity, vorticity_obs, text_obj, step_text, fig
-    end#Plot_Vorticity
-
-    function Create_Plot(gridlengthX::Int64, gridlengthY::Int64, velocityX::Array{Float64, 2}, Direction::String)
-                ## Set up the figure and axis
-        # Initialize Plot arrays
-        velocity_obs = Observable(velocityX);       
-        # Set up the figure and axis with explicit sizing
-        fig = Figure(size = (1000, 400))
-        ax = Axis(fig[1, 1], aspect = DataAspect(), title = "Velocity_$Direction")
-
-        # Display the vorticity field using a heatmap with dynamic color range
-        hm = heatmap!(ax, 1:gridlengthX, 1:gridlengthY, velocity_obs, 
-                    colormap = :inferno, 
-                    nan_color = :black,
-                    colorrange = (-0.2, 0.2))
-        Colorbar(fig[1, 2], hm, label = "Lattice_Velocity_$Direction")
-        rowsize!(fig.layout, 1, ax.scene.viewport[].widths[2])
-        # Set axis limits explicitly
-        xlims!(ax, 1, gridlengthX)
-        ylims!(ax, 1, gridlengthY)
-
-        # Create a text element for time step display
-        step_text = Observable("Time step: 0, 0s")
-        text_obj = text!(ax, ceil(Int, (gridlengthX*2/100)), ceil(Int, (gridlengthY*2/100)), text = step_text, 
-                color = :black, fontsize = 14)
-
-                return velocity_obs, text_obj, step_text, fig
-    end#Plot_vx
-
-    function Create_Plot3D(nx::Int, ny::Int, nz::Int, field3d::Array{<:Real,3}; title="Test123")
-        # Initialize Plot arrays
-        vol_obs = Observable(Float32.(field3d))
-        # Set up the figure and axis with explicit sizing
-        fig = Figure(size = (900, 700))
-        ax  = Axis3(fig[1, 1], title = title)
-
-        plt = volume!(ax, vol_obs; colormap = :turbo, transparency = true)
-        Colorbar(fig[1, 2], plt, label = title)
-
-        #Create a text element for time step display
-        step_text = Observable("Time step: 0, 0s")
-        text!(ax, 1, 1, nz, text = step_text, color = :white, fontsize = 22, align = (:left, :bottom))
-
-        return vol_obs, step_text, fig
+    function transparency_map(n::Int=256)
+        t = range(-1, 1; length = n)
+        alpha = 0.05 .+ 0.95 .* abs.(t)
+        Makie.cgrad(:balance, n, alpha=alpha)
     end
+
+    function transparency_map_vorticity(n::Int=256)
+        t=range(0, 1; length=n)
+        alpha = 0.15 .+ 0.85 .* (t .^ 0.6)
+        Makie.cgrad(:turbo, n, alpha=alpha)
+    end
+######################################################
+
+    function Create_Plot_XY(nx::Int, ny::Int, field2d::Array{<:Real, 2}; width=900, height=400, title="vx slice XY")
+        vx_obs = Observable(field2d)
+        fig = Figure(size = (width, height))
+        ax = Axis(fig[1,1], aspect = DataAspect(), title = title)
+        colorrange =(-0.2, 0.2)
+        hm = heatmap!(ax, 1:nx, 1:ny, vx_obs; 
+                        colormap = transparency_map(), 
+                        nan_color= :black, colorrange = colorrange, 
+                        transparency=true)
+
+        Colorbar(fig[1, 2], hm, label = "Lattice_Velocity")
+        xlims!(ax, 1, nx); ylims!(ax, 1, ny)
+        step_text = Observable("Time step: 0, 0s")
+        text!(ax, ceil(Int, nx*0.05), ceil(Int, ny*0.05), text = step_text, color = :black, fontsize = 14)
+        return vx_obs, step_text, fig
+    end
+
+    function Create_Plot_XZ(nx::Int, nz::Int, field2d::Array{<:Real, 2}; width=900, height=400, title="vx slice XZ")
+        vx_obs = Observable(field2d)
+        fig = Figure(size = (width, height))
+        ax = Axis(fig[1,1], aspect = DataAspect(), title = title)
+        hm = heatmap!(ax, 1:nx, 1:nz, vx_obs; colormap = :inferno, nan_color= :white, colorrange = (-0.2, 0.2))
+        Colorbar(fig[1, 2], hm, label = "Lattice_Velocity")
+        xlims!(ax, 1, nx); ylims!(ax, 1, nz)
+        step_text = Observable("Time step: 0, 0s")
+        text!(ax, ceil(Int, nx*0.05), ceil(Int, nz*0.05), text = step_text, color = :black, fontsize = 14)
+        return vx_obs, step_text, fig
+    end
+
+    function Create_Vorticity_XY(nx::Int, ny::Int, vorticity_XY::Array{<:Real, 2}; width=900, height=400, title="|ω| slice XY")
+        omega_obs = Observable(vorticity_XY)
+        fig = Figure(size = (width, height))
+        ax = Axis(fig[1,1], aspect = DataAspect(), title = title)
+        hm = heatmap!(ax, 1:nx, 1:ny, omega_obs;
+                      colormap = transparency_map_vorticity(),
+                      nan_color = :black,
+                      colorrange = (0.0, 0.05),
+                      transparency = true)
+        Colorbar(fig[1,2], hm, label = "|ω| (lattice)")
+        xlims!(ax, 1, nx); ylims!(ax, 1, ny)
+        step_text = Observable("Time step: 0, 0s")
+        text!(ax, ceil(Int, nx*0.05), ceil(Int, ny*0.05), text = step_text, color = :black, fontsize = 14)
+        
+        return omega_obs, step_text, fig
+    end
+
+    function Create_Vorticity_XZ(nx::Int, nz::Int, vorticity_XZ::Array{<:Real, 2}; width=900, height=400, title="|ω| slice XZ")
+        omega_obs = Observable(vorticity_XZ)
+        fig = Figure(size = (width, height))
+        ax = Axis(fig[1,1], aspect = DataAspect(), title = title)
+        hm = heatmap!(ax, 1:nx, 1:nz, omega_obs;
+                      colormap = transparency_map_vorticity(),
+                      nan_color = :black,
+                      colorrange = (0.0, 0.05),
+                      transparency = true)
+        Colorbar(fig[1,2], hm, label = "|ω| (lattice)")
+        xlims!(ax, 1, nx); ylims!(ax, 1, nz)
+        step_text = Observable("Time step: 0, 0s")
+        text!(ax, ceil(Int, nx*0.05), ceil(Int, nz*0.05), text = step_text, color = :black, fontsize = 14)
+        
+        return omega_obs, step_text, fig
+    end
+
+
+
+
+
+
+
+
+
+    # function Create_Plot3D(nx::Int, ny::Int, nz::Int, field3d::Array{<:Real,3}; title="Test123")
+    #     # Initialize Plot arrays
+    #     vol_obs = Observable(Float32.(field3d))
+    #     # Set up the figure and axis with explicit sizing
+    #     fig = Figure(size = (900, 700))
+    #     ax  = Axis3(fig[1, 1], title = title)
+
+    #     plt = volume!(ax, vol_obs; colormap = :turbo, transparency = true, colorrange = (0f0, 50f0))
+    #     Colorbar(fig[1, 2], plt, label = title)
+
+    #     #Create a text element for time step display
+    #     step_text = Observable("Time step: 0, 0s")
+    #     text!(ax, 1, 1, nz, text = step_text, color = :white, fontsize = 22, align = (:left, :bottom))
+
+    #     return vol_obs, step_text, fig
+    # end
+
+    #############################################################################
+    # function Create_Plot(gridlengthX::Int64, gridlengthY::Int64)
+    #     ## Set up the figure and axis
+    #     # Initialize Plot arrays
+    #     vorticity = zeros(gridlengthX, gridlengthY);
+    #     vorticity_obs = Observable(vorticity);       
+    #     # Set up the figure and axis with explicit sizing
+    #     fig = Figure(size = (1000, 400))
+    #     ax = Axis(fig[1, 1], aspect = DataAspect(), title = "Vorticity")
+
+    #     # Display the vorticity field using a heatmap with dynamic color range
+    #     hm = heatmap!(ax, 1:gridlengthX, 1:gridlengthY, vorticity_obs, 
+    #                 colormap = :curl, 
+    #                 nan_color = :black,
+    #                 colorrange = (-0.2, 0.2))
+    #     Colorbar(fig[1, 2], hm, label = "Lattice_Vorticity")
+    #     rowsize!(fig.layout, 1, ax.scene.viewport[].widths[2])
+    #     # Set axis limits explicitly
+    #     xlims!(ax, 1, gridlengthX)
+    #     ylims!(ax, 1, gridlengthY)
+
+    #     # Create a text element for time step display
+    #     step_text = Observable("Time step: 0, 0s")
+    #     text_obj = text!(ax, ceil(Int, (gridlengthX*2/100)), ceil(Int, (gridlengthY*2/100)), text = step_text, 
+    #             color = :black, fontsize = 14)
+    #             return vorticity, vorticity_obs, text_obj, step_text, fig
+    # end#Plot_Vorticity
+
+    # function Create_Plot(gridlengthX::Int64, gridlengthY::Int64, velocityX::Array{Float64, 2}, Direction::String)
+    #             ## Set up the figure and axis
+    #     # Initialize Plot arrays
+    #     velocity_obs = Observable(velocityX);       
+    #     # Set up the figure and axis with explicit sizing
+    #     fig = Figure(size = (1000, 400))
+    #     ax = Axis(fig[1, 1], aspect = DataAspect(), title = "Velocity_$Direction")
+
+    #     # Display the vorticity field using a heatmap with dynamic color range
+    #     hm = heatmap!(ax, 1:gridlengthX, 1:gridlengthY, velocity_obs, 
+    #                 colormap = :inferno, 
+    #                 nan_color = :black,
+    #                 colorrange = (-0.2, 0.2))
+    #     Colorbar(fig[1, 2], hm, label = "Lattice_Velocity_$Direction")
+    #     rowsize!(fig.layout, 1, ax.scene.viewport[].widths[2])
+    #     # Set axis limits explicitly
+    #     xlims!(ax, 1, gridlengthX)
+    #     ylims!(ax, 1, gridlengthY)
+
+    #     # Create a text element for time step display
+    #     step_text = Observable("Time step: 0, 0s")
+    #     text_obj = text!(ax, ceil(Int, (gridlengthX*2/100)), ceil(Int, (gridlengthY*2/100)), text = step_text, 
+    #             color = :black, fontsize = 14)
+
+    #             return velocity_obs, text_obj, step_text, fig
+    # end#Plot_vx
+
 ## Update Plot
 
 end#module
