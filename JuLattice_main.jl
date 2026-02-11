82280acb8d5bdb0998508911f7433b77c24c0d6d
Sören Blumberg <soeren.blumberg@tu-braunschweig.de>
2026-02-11 11:15:27 +0100

    added bounce-back BC with stream-back from solid-nodes; added periodic Inlet / Outlet BC

--- JuLattice_main.jl
+++ JuLattice_main.jl
@@ -441,52 +441,56 @@ function run_JuLattice()
         end
 
         ###### (new) Boundary Conditions ######
-        # periodic inlet/outlet
-        # right side
-        fp00S[2, 2:gridlengthY-1, gridlengthZ-1] .= fp00S[gridlengthX, 2:gridlengthY-1, gridlengthZ-1]
-        fp0pS[2, 2:gridlengthY-1, gridlengthZ-1] .= fp0pS[gridlengthX, 2:gridlengthY-1, gridlengthZ-1]
-        fp0mS[2, 2:gridlengthY-1, gridlengthZ-1] .= fp0mS[gridlengthX, 2:gridlengthY-1, gridlengthZ-1]
-        fpm0S[2, 2:gridlengthY-1, gridlengthZ-1] .= fpm0S[gridlengthX, 2:gridlengthY-1, gridlengthZ-1]
-        fpp0S[2, 2:gridlengthY-1, gridlengthZ-1] .= fpp0S[gridlengthX, 2:gridlengthY-1, gridlengthZ-1]
-        
-        # left side
-        fm00S[gridlengthX-1, 2:gridlengthY-1, gridlengthZ-1] .= fm00S[1, 2:gridlengthY-1, gridlengthZ-1]
-        fmm0S[gridlengthX-1, 2:gridlengthY-1, gridlengthZ-1] .= fmm0S[1, 2:gridlengthY-1, gridlengthZ-1]
-        fmp0S[gridlengthX-1, 2:gridlengthY-1, gridlengthZ-1] .= fmp0S[1, 2:gridlengthY-1, gridlengthZ-1]
-        fm0mS[gridlengthX-1, 2:gridlengthY-1, gridlengthZ-1] .= fm0mS[1, 2:gridlengthY-1, gridlengthZ-1]
-        fm0pS[gridlengthX-1, 2:gridlengthY-1, gridlengthZ-1] .= fm0pS[1, 2:gridlengthY-1, gridlengthZ-1]
 
-        # Bounceback walls
+        ## periodic inlet/outlet
+        # INLET  (left side)
+        fp00S[2, 2:gridlengthY-1, 2:gridlengthZ-1] .= fp00S[gridlengthX, 2:gridlengthY-1, 2:gridlengthZ-1]
+        fp0pS[2, 2:gridlengthY-1, 3:gridlengthZ-1] .= fp0pS[gridlengthX, 2:gridlengthY-1, 3:gridlengthZ-1]
+        fp0mS[2, 2:gridlengthY-1, 2:gridlengthZ-2] .= fp0mS[gridlengthX, 2:gridlengthY-1, 2:gridlengthZ-2]
+        fpm0S[2, 2:gridlengthY-2, 2:gridlengthZ-1] .= fpm0S[gridlengthX, 2:gridlengthY-2, 2:gridlengthZ-1]
+        fpp0S[2, 3:gridlengthY-1, 2:gridlengthZ-1] .= fpp0S[gridlengthX, 3:gridlengthY-1, 2:gridlengthZ-1]
         
+        # OUTLET (right side)
+        fm00S[gridlengthX-1, 2:gridlengthY-1, 2:gridlengthZ-1] .= fm00S[1, 2:gridlengthY-1, 2:gridlengthZ-1]
+        fm0pS[gridlengthX-1, 2:gridlengthY-1, 3:gridlengthZ-1] .= fm0pS[1, 2:gridlengthY-1, 3:gridlengthZ-1]
+        fm0mS[gridlengthX-1, 2:gridlengthY-1, 2:gridlengthZ-2] .= fm0mS[1, 2:gridlengthY-1, 2:gridlengthZ-2]
+        fmm0S[gridlengthX-1, 2:gridlengthY-2, 2:gridlengthZ-1] .= fmm0S[1, 2:gridlengthY-2, 2:gridlengthZ-1]
+        fmp0S[gridlengthX-1, 3:gridlengthY-1, 2:gridlengthZ-1] .= fmp0S[1, 3:gridlengthY-1, 2:gridlengthZ-1] 
+     
+
+        ## Bounceback walls
         # bottom wall (z=1)
-        f00pS[2:gridlengthX-1, 2:gridlengthY-1, 2] .= f00mS[2:gridlengthX-1, 2:gridlengthY-1, 1] #mitte
-        f0mpS[2:gridlengthX-1, 2:gridlengthY-1, 2] .= f0pmS[2:gridlengthX-1, 2:gridlengthY-1, 1] #4 kanten unten -> oben
-        fm0pS[2:gridlengthX-1, 2:gridlengthY-1, 2] .= fp0mS[2:gridlengthX-1, 2:gridlengthY-1, 1] 
-        f0ppS[2:gridlengthX-1, 2:gridlengthY-1, 2] .= f0mmS[2:gridlengthX-1, 2:gridlengthY-1, 1]
-        fp0pS[2:gridlengthX-1, 2:gridlengthY-1, 2] .= fm0mS[2:gridlengthX-1, 2:gridlengthY-1, 1]
+        f00pS[2:gridlengthX-1, 2:gridlengthY-1, 2] .= f00mS[2:gridlengthX-1, 2:gridlengthY-1, 1]    # mitte
+        f0ppS[2:gridlengthX-1, 2:gridlengthY-1, 2] .= f0mmS[2:gridlengthX-1, 1:gridlenghtY-2, 1]
+        f0mpS[2:gridlengthX-1, 2:gridlengthY-1, 2] .= f0pmS[2:gridlengthX-1, 3:gridlenghtY, 1]
+        fp0pS[2:gridlengthX-1, 2:gridlengthY-1, 2] .= fm0mS[1:gridlengthX-2, 2:gridlengthY-1, 1]
+        fm0pS[2:gridlengthX-1, 2:gridlengthY-1, 2] .= fp0mS[3:gridlengthX, 2:gridlengthY-1, 1]
 
         # top wall (z=gridlengthZ)
-        f00mS[2:gridlengthX-1, 2:gridlengthY-1, gridlengthZ-1] .= f00pS[2:gridlenghtX-1, 2:gridlengthY-1, gridlengthZ]  #mitte
-        f0pmS[2:gridlengthX-1, 2:gridlengthY-1, gridlengthZ-1] .= f0mpS[2:gridlenghtX-1, 2:gridlengthY-1, gridlengthZ]  #4 kanten oben -> unten
-        fp0mS[2:gridlengthX-1, 2:gridlengthY-1, gridlengthZ-1] .= fm0pS[2:gridlenghtX-1, 2:gridlengthY-1, gridlengthZ]  
-        f0mmS[2:gridlengthX-1, 2:gridlengthY-1, gridlengthZ-1] .= f0ppS[2:gridlenghtX-1, 2:gridlengthY-1, gridlengthZ]
-        fm0mS[2:gridlengthX-1, 2:gridlengthY-1, gridlengthZ-1] .= fp0pS[2:gridlenghtX-1, 2:gridlengthY-1, gridlengthZ]
+        f00mS[2:gridlengthX-1, 2:gridlengthY-1, gridlengthZ-1] .= f00pS[2:gridlengthX-1, 2:gridlengthY-1, gridlengthZ]  # mitte
+        f0pmS[2:gridlengthX-1, 2:gridlengthY-1, gridlengthZ-1] .= f0mpS[2:gridlengthX-1, 1:gridlengthY-2, gridlengthZ]
+        f0mmS[2:gridlengthX-1, 2:gridlengthY-1, gridlengthZ-1] .= f0ppS[2:gridlengthX-1, 3:gridlengthY, gridlengthZ]
+        fp0mS[2:gridlengthX-1, 2:gridlengthY-1, gridlengthZ-1] .= fm0pS[1:gridlengthX-2, 2:gridlenghtY-1, gridlengthZ]
+        fm0mS[2:gridlengthX-1, 2:gridlengthY-1, gridlengthZ-1] .= fp0pS[3:gridlengthX, 2:gridlengthY-1, gridlengthZ]
+
 
         # front wall (y=1)
-        f0m0S[2:gridlengthX-1, 2, gridlengthZ-1] .= f0p0S[2:gridlengthX-1, 1, gridlengthZ-1]   #mitte
-        f0mpS[2:gridlengthX-1, 2, gridlengthZ-1] .= f0pmS[2:gridlengthX-1, 1, gridlengthZ-1]   #4 kanten
-        fpm0S[2:gridlengthX-1, 2, gridlengthZ-1] .= fmp0S[2:gridlengthX-1, 1, gridlengthZ-1]
-        f0mmS[2:gridlengthX-1, 2, gridlengthZ-1] .= f0ppS[2:gridlengthX-1, 1, gridlengthZ-1]
-        fmm0S[2:gridlengthX-1, 2, gridlengthZ-1] .= fpp0S[2:gridlengthX-1, 1, gridlengthZ-1]
+        f0p0S[2:gridlengthX-1, 2, 2:gridlengthZ-1] .= f0m0S[2:gridlengthX-1, 1, 2:gridlengthZ-1]
+        fpp0S[2:gridlengthX-1, 2, 2:gridlengthZ-1] .= fmm0S[1:gridlengthX-2, 1, 2:gridlengthZ-1]
+        fmp0S[2:gridlengthX-1, 2, 2:gridlengthZ-1] .= fpm0S[3:gridlengthZ, 1, 2:gridlengthZ-1]
+
+            # double counted populations (special handling at top/bot)
+        f0pmS[2:gridlengthX-1, 2, 2:gridlengthZ-2] .= f0mpS[2:gridlengthX-1, 1, 3:gridlengthZ-1]
+        f0ppS[2:gridlengthX-1, 2, 3:gridlengthZ-1] .= f0mmS[2:gridlengthX-1, 1, 2:gridlengthY-2]
 
         # back wall (y=gridlengthY)
-        f0p0S[2:gridlengthX-1, gridlengthY-1, gridlengthZ-1] .= f0m0S[2:gridlengthX-1, gridlengthY, gridlengthZ-1]   #mitte
-        f0pmS[2:gridlengthX-1, gridlengthY-1, gridlengthZ-1] .= f0mpS[2:gridlengthX-1, gridlengthY, gridlengthZ-1]   #4 kanten
-        fmp0S[2:gridlengthX-1, gridlengthY-1, gridlengthZ-1] .= fpm0S[2:gridlengthX-1, gridlengthY, gridlengthZ-1]
-        f0ppS[2:gridlengthX-1, gridlengthY-1, gridlengthZ-1] .= f0mmS[2:gridlengthX-1, gridlengthY, gridlengthZ-1]
-        fpp0S[2:gridlengthX-1, gridlengthY-1, gridlengthZ-1] .= fmm0S[2:gridlengthX-1, gridlengthY, gridlengthZ-1]
+        f0m0S[2:gridlengthX-1, gridlengthY-1, 2:gridlengthZ-1] .= f0p0S[2:gridlengthX-1, gridlengthY, 2:gridlengthZ-1]
+        fpm0S[2:gridlengthX-1, gridlengthY-1, 2:gridlengthZ-1] .= fmp0S[1:gridlengthX-2, gridlengthY, 2:gridlengthZ-1]
+        fmm0S[2:gridlengthX-1, gridlengthY-1, 2:gridlengthZ-1] .= fpp0S[3:gridlengthX, gridlengthY, 2:gridlengthZ-1]
 
-        # Edges and Corners Bounceback
+            # double counted populations (special handling at top/bot)
+        f0mmS[2:gridlengthX-1, gridlengthY-1, 2:gridlengthZ-2] .= f0ppS[2:gridlengthX-1, gridlengthY, 3:gridlengthZ-1]
+        f0mpS[2:gridlengthX-1, gridlengthY-1, 3:gridlengthZ-1] .= f0pmS[2:gridlengthX-1, gridlengthY, 2:gridlengthZ-2]
         
 
 
@@ -499,7 +503,7 @@ function run_JuLattice()
 
         # XY-plane edges (vectorized)
         fpp0S[sphere_indices], fmm0S[sphere_indices] = fmm0S[sphere_indices], fpp0S[sphere_indices]
-        fpm0S[sphere_indices], fmp0S[sphere_indices] = fmp0S[sphere_indices], fpm0S[sphere_indices]
+        fpm0S[sphere_indices], po[sphere_indices] = fmp0S[sphere_indices], fpm0S[sphere_indices]
 
         # XZ-plane edges (vectorized)
         fp0pS[sphere_indices], fm0mS[sphere_indices] = fm0mS[sphere_indices], fp0pS[sphere_indices]
@@ -509,27 +513,6 @@ function run_JuLattice()
         f0ppS[sphere_indices], f0mmS[sphere_indices] = f0mmS[sphere_indices], f0ppS[sphere_indices]
         f0pmS[sphere_indices], f0mpS[sphere_indices] = f0mpS[sphere_indices], f0pmS[sphere_indices]
         
-        # for idx in sphere_indices
-        #     x, y, z = Tuple(idx)    
-
-        #     # Cell faces - swap
-        #     fp00S[x,y,z], fm00S[x,y,z] = fm00S[x,y,z], fp00S[x,y,z]  # X-Richtung
-        #     f0p0S[x,y,z], f0m0S[x,y,z] = f0m0S[x,y,z], f0p0S[x,y,z]  # Y-Richtung
-        #     f00pS[x,y,z], f00mS[x,y,z] = f00mS[x,y,z], f00pS[x,y,z]  # Z-Richtung
-
-        #     # XY-plane edges
-        #     fpp0S[x,y,z], fmm0S[x,y,z] = fmm0S[x,y,z], fpp0S[x,y,z]   
-        #     fpm0S[x,y,z], fmp0S[x,y,z] = fmp0S[x,y,z], fpm0S[x,y,z]   
-
-        #     # XZ-plane edges
-        #     fp0pS[x,y,z], fm0mS[x,y,z] = fm0mS[x,y,z], fp0pS[x,y,z]  
-        #     fp0mS[x,y,z], fm0pS[x,y,z] = fm0pS[x,y,z], fp0mS[x,y,z]  
-
-        #     # YZ-plane edges
-        #     f0ppS[x,y,z], f0mmS[x,y,z] = f0mmS[x,y,z], f0ppS[x,y,z]  
-        #     f0pmS[x,y,z], f0mpS[x,y,z] = f0mpS[x,y,z], f0pmS[x,y,z]
-        # end 
-        ###### (new) Boundary Conditions ######
 
         # Swap: SWAP POINTERS new distribution to "old"
         f000, f000S = f000S, f000
