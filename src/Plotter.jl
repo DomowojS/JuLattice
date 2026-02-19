6dbfd1584dd04cf83374364250f59b6b14dc2c36
Daniel Sukhman <daniel.sukhman@hvl.no>
2026-02-19 23:40:23 +0100

    add physical plotting

--- src/Plotter.jl
+++ src/Plotter.jl
@@ -2,7 +2,10 @@ module Plotter
 using GLMakie
 export Create_Plot, Update_Plot!
 
-function Create_Plot(Nx::Int, Ny::Int, plotU::Bool, plotV::Bool, plotVorticity::Bool)
+function Create_Plot(Nx::Int, Ny::Int, plotU::Bool, plotV::Bool, plotVorticity::Bool,
+                     rangeU::Tuple{Float64,Float64},
+                     rangeV::Tuple{Float64,Float64},
+                     rangeVort::Tuple{Float64,Float64})
     nplots = Int(plotU) + Int(plotV) + Int(plotVorticity)
     fig = Figure(size = (900 * max(nplots, 1), 420))
 
@@ -13,26 +16,26 @@ function Create_Plot(Nx::Int, Ny::Int, plotU::Bool, plotV::Bool, plotVorticity::
     col = 1
 
     if plotU
-        ax = Axis(fig[1, col], title = "U  (x-velocity)", aspect = DataAspect())
+        ax = Axis(fig[1, col], title = "U  [m/s]", aspect = DataAspect())
         obs_u = Observable(zeros(Nx, Ny))
-        hm = heatmap!(ax, obs_u, colormap = :inferno, colorrange = (-0.08, 0.08), nan_color = :black)
+        hm = heatmap!(ax, obs_u, colormap = :inferno, colorrange = rangeU, nan_color = :black)
         Colorbar(fig[2, col], hm, vertical = false)
         text!(ax, 2, 2, text = step_text, color = :white, fontsize = 11)
         col += 1
     end
 
     if plotV
-        ax = Axis(fig[1, col], title = "V  (y-velocity)", aspect = DataAspect())
+        ax = Axis(fig[1, col], title = "V  [m/s]", aspect = DataAspect())
         obs_v = Observable(zeros(Nx, Ny))
-        hm = heatmap!(ax, obs_v, colormap = :inferno, colorrange = (-0.04, 0.04), nan_color = :black)
+        hm = heatmap!(ax, obs_v, colormap = :inferno, colorrange = rangeV, nan_color = :black)
         Colorbar(fig[2, col], hm, vertical = false)
         col += 1
     end
 
     if plotVorticity
-        ax = Axis(fig[1, col], title = "Vorticity", aspect = DataAspect())
+        ax = Axis(fig[1, col], title = "Vorticity  [1/s]", aspect = DataAspect())
         obs_vort = Observable(zeros(Nx, Ny))
-        hm = heatmap!(ax, obs_vort, colormap = :curl, colorrange = (-0.05, 0.05), nan_color = :black)
+        hm = heatmap!(ax, obs_vort, colormap = :curl, colorrange = rangeVort, nan_color = :black)
         Colorbar(fig[2, col], hm, vertical = false)
         col += 1
     end
@@ -42,28 +45,30 @@ end
 
 function Update_Plot!(obs_u, obs_v, obs_vort, step_text,
                       velocityX, velocityY,
-                      i, deltaT,
+                      i, deltaT, deltaX,
                       plotU::Bool, plotV::Bool, plotVorticity::Bool,
                       isFluid::BitMatrix)
+
+    velScale  = deltaX / deltaT          # [lu/ts] -> [m/s]
+    vortScale = 1.0 / (2.0 * deltaT)    # central-diff (no /2) -> [1/s]
 
     step_text[] = "Step: $i  |  t = $(round(i*deltaT, digits=2)) s"
 
     if plotU && obs_u !== nothing
-        obs_u[] = copy(velocityX)
+        obs_u[] = velocityX .* velScale
     end
 
     if plotV && obs_v !== nothing
-        obs_v[] = copy(velocityY)
+        obs_v[] = velocityY .* velScale
     end
 
     if plotVorticity && obs_vort !== nothing
         vort = zeros(size(velocityX))
-        # Central difference over fluid nodes (works for arbitrary fluid masks)
         for I in CartesianIndices(isFluid)
             if isFluid[I]
                 ix, iy = I.I
-                vort[I] = (velocityY[ix+1, iy] - velocityY[ix-1, iy]) -
-                           (velocityX[ix, iy+1] - velocityX[ix, iy-1])
+                vort[I] = ((velocityY[ix+1, iy] - velocityY[ix-1, iy]) -
+                            (velocityX[ix, iy+1] - velocityX[ix, iy-1])) * vortScale
             end
         end
         obs_vort[] = vort
