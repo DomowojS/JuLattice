1e6fe89a0dbd7bc0ba963f69f40c5dd24d814d20
Sören Blumberg <soeren.blumberg@tu-braunschweig.de>
2026-05-20 17:29:35 +0200

    added julia starting with threading activated for .ps1 files

--- queue_LabPC.ps1
+++ queue_LabPC.ps1
@@ -1,9 +1,9 @@
 # Run from: C:\...\JuLattice\
 
 Write-Host "Starting simulation 1: Grid Study D/dx=25"
-julia run_gridStudy_Ddeltax25.jl
+julia -t 18 run_gridStudy_Ddeltax25.jl
 
 Write-Host "Starting simulation 2: Domain Extension D/dx=20"
-julia run_domainExtension_Ddeltax20.jl
+julia -t 18 run_domainExtension_Ddeltax20.jl
 
 Write-Host "All simulations complete."
